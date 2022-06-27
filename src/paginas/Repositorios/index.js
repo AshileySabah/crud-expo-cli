4d7ebcbc13ef6f7fe764551d6126ffd4cb1e5b64
AshileySabah <ashileysabrina2002@gmail.com>
2022-06-27 11:43:21 -0300

    feat: listar respositorios do usuario

--- src/paginas/Repositorios/index.js
+++ src/paginas/Repositorios/index.js
@@ -1,9 +1,15 @@
 import React, {useEffect, useState} from 'react';
 import {Text, View, FlatList, TouchableOpacity} from 'react-native';
 import estilos from './estilos';
+import {pegarRepositoriosDoUsuario} from '../../servicos/requisicoes/repositorios';
 
 export default function Repositorios({route, navigation}) {
   const [repo, setRepo] = useState([]);
+
+  useEffect(async () => {
+    const resultado = await pegarRepositoriosDoUsuario(route.params.id);
+    setRepo(resultado);
+  }, []);
 
   return (
     <View style={estilos.container}>
@@ -15,6 +21,22 @@ export default function Repositorios({route, navigation}) {
         onPress={() => navigation.navigate('CriarRepositorio')}>
         <Text style={estilos.textoBotao}>Adicionar novo repositório</Text>
       </TouchableOpacity>
+
+      <FlatList
+        data={repo}
+        style={{width: '100%'}}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => (
+          <TouchableOpacity
+            style={estilos.repositorio}
+            onPress={() => navigation.navigate('InfoRepositorio', {item})}>
+            <Text style={estilos.repositorioNome}>{item.name}</Text>
+            <Text style={estilos.repositorioData}>
+              Atualizado em {item.data}
+            </Text>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 }
